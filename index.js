1375d8e501eb5b94775b78c6d89f9c97f67b2327
Matias Walter <matiwalter97@hotmail.com>
2021-05-11 20:24:15 -0300

    added offcanvas, projects and skills sections

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-// const navLinks = document.querySelectorAll('.nav-item')
-// const menuToggle = document.getElementById('navbarNav')
-// const bsCollapse = new bootstrap.Collapse(menuToggle)
-// navLinks.forEach((l) => {
-//     l.addEventListener('click', () => { bsCollapse.toggle() })
-// })
+document.addEventListener("DOMContentLoaded", function(){
+  var navMenu = document.getElementById('offcanvas');
+  var bsOffcanvas = new bootstrap.Offcanvas(navMenu);
+  const toggleOffcanvas = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    bsOffcanvas.toggle();
+  }
+  document.getElementById("OpenMenu").addEventListener('click',function (e){
+    e.preventDefault();
+    e.stopPropagation();
+    bsOffcanvas.toggle();
+  });
+
+  // document.getElementById("screen").addEventListener('click', (e) => {
+  //   e.preventDefault();
+  //   e.stopPropagation();
+  //   bsOffcanvas.toggle();
+  // });
+})
