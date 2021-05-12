339ed00562f734a4d8fceb19571e0370cf02d2d1
Matias Walter <matiwalter97@hotmail.com>
2021-05-12 01:24:16 -0300

    close navbar on button click added

--- offcanvas.js
+++ offcanvas.js
@@ -2,7 +2,14 @@
     'use strict'
   
     document.querySelector('[data-bs-toggle="offcanvas"]').addEventListener('click', function () {
-      document.querySelector('.offcanvas-collapse').classList.toggle('open')
+      document.querySelector('.offcanvas-collapse').classList.remove('close');
+      document.querySelector('.offcanvas-collapse').classList.toggle('open');
     })
   })()
-  
+
+const a = document.querySelectorAll('[data-bs-dismiss="offcanvas"]');
+const offcanvas = document.querySelector('.offcanvas-collapse');
+a.forEach( n => n.addEventListener('click', () => {
+  offcanvas.classList.remove('open');
+  offcanvas.classList.add('close');
+}));
